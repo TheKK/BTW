2b685ce202843933bbd48cd8612dc1595a1378df
TheKK <thumbd03803@gmail.com>
2014-11-23 17:00:27 +0800

    Rename "testGameActor" to "gameActor_Zup"

---
+++ include/gameActor_Zup.h
@@ -0,0 +1,44 @@
+/*
+ * Author: KK <thumbd03803@gmail.com>
+ *
+ * File: gameActor_Zup.h
+ */
+
+#ifndef GAME_ACTOR_ZUP_H
+#define GAME_ACTOR_ZUP_H
+
+#include <iostream>
+
+#include "gameActor.h"
+#include "testStateMachine.h"
+#include "sprite.h"
+#include "window.h"
+
+using namespace std;
+
+class GameActor_Zup : public GameActor
+{
+public:
+	GameActor_Zup();
+	~GameActor_Zup();
+
+	void handleInput(const GameActorController& controller);
+	void update();
+	void render();
+
+	void updatePosAndSprite();
+	void moveRight();
+	void moveLeft();
+private:
+	Sprite sprite_onGround_;
+	//Sprite sprite_onGroundAttack_;
+	Sprite sprite_jumping_;
+	//Sprite sprite_jumpingAttack_;
+
+	TestStateMachine machine_;
+
+	int frictionDelay_;
+	int spriteDelay_;
+};
+
+#endif /* GAME_ACTOR_ZUP_H */
---
+++ src/gameActor_Zup.cpp
@@ -0,0 +1,94 @@
+/*
+ * Author: KK <thumbd03803@gmail.com>
+ *
+ * File: gameActor_Zup.cpp
+ */
+
+#include "gameActor_Zup.h"
+
+GameActor_Zup::GameActor_Zup():
+	sprite_onGround_("./game/images/zup_onGround.png", Window::renderer(),
+			 30, 65),
+	sprite_jumping_("./game/images/zup_jumping.png", Window::renderer(),
+			30, 65),
+	machine_(),
+	frictionDelay_(0),
+	spriteDelay_(0)
+{
+	posRect_ = {0, 0, 30, 65};
+	setGravity(1);
+	setHorizon(Window::height() - 70);
+
+	spriteList_[SPRITE_ON_GROUND] = &sprite_onGround_;
+	spriteList_[SPRITE_JUMPING] = &sprite_jumping_;
+	currentSprite_ = spriteList_[SPRITE_ON_GROUND];
+
+	machine_.onEnter(*this);
+}
+
+GameActor_Zup::~GameActor_Zup()
+{
+}
+
+void
+GameActor_Zup::handleInput(const GameActorController& controller)
+{
+	machine_.handleInput(*this, controller);
+}
+
+void
+GameActor_Zup::update()
+{
+	updatePosAndSprite();
+
+	machine_.update(*this);
+
+	updateBullet(*this);
+}
+
+void
+GameActor_Zup::render()
+{
+	currentSprite_->render(posRect_);
+
+	renderBullet();
+}
+
+void
+GameActor_Zup::updatePosAndSprite()
+{
+	moveBy(velX_, velY_);
+	if (!isOnGround())
+		velY_ += gravity_;
+
+	if (posRect_.y + posRect_.h > horizon_)
+		posRect_.y = horizon_ - posRect_.h;
+
+	if (velX_ != 0) {
+		if (++frictionDelay_ == 4) {
+			velX_ -= velX_ * 0.5;
+			frictionDelay_ = 0;
+		}
+	}
+
+	if (++spriteDelay_ == 10) {
+		currentSprite_->nextFrame();
+		spriteDelay_ = 0;
+	}
+}
+
+void
+GameActor_Zup::moveRight()
+{
+	applyAcc(1, 0);
+	direction_ = ACTOR_FACE_RIGHT;
+	currentSprite_->setFlip(FLIP_NONE);
+}
+
+void
+GameActor_Zup::moveLeft()
+{
+	applyAcc(-1, 0);
+	direction_ = ACTOR_FACE_LEFT;
+	currentSprite_->setFlip(FLIP_HORIZONTAL);
+}
